3bad989b1cdc45d1e32b65ced947f38fdec6a207
Yashas Chandra <yashas998@gmail.com>
2023-09-14 14:37:23 +0530

    Customizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        // primary: "#050816",
+        // secondary: "#aaa6c3",
+        // tertiary: "#151030",
+        primary: "#DDC3A5",
+        secondary: "#201E20",
+        tertiary: "#E0A96D",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
@@ -19,7 +22,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/topbg.jpg')",
       },
     },
   },
